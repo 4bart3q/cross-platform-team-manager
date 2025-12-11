9b142419d0dd9319ef5ffcbbc970118c21b3c5d0
Bartek <bartekg0804@gmail.com>
2025-12-11 15:09:22 +0100

    Aktualizacja punktu startowego, uruchamianie dwóch botów jednocześnie

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from bots.discord_bot import run_discord_bot 
+from bots.discord_bot import run_discord_bot
+from bots.telegram_bot import run_telegram_bot 
+import threading
 
 if __name__ == "__main__":
+    
+    # Uruchamianie bota Telegrama w osobnym wątku
+    telegram_thread = threading.Thread(target=run_telegram_bot)
+    telegram_thread.start()
+    
+    # Uruchamianie bota Discorda w głównym wątku
     run_discord_bot()
